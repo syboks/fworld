45eb96054e69377086434da00ca25674b5d15e72
syboks <nhidzab@gmail.com>
2025-06-20 22:36:58 +0200

    first commit

---
+++ src/main/java/com/syboks/fworld/FworldApplication.java
@@ -0,0 +1,13 @@
+package com.syboks.fworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syboks/fworld/FworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syboks.fworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
